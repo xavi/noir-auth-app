f65f4472fa5417d2404447005fc37ab6252449ae
xavi <xavi@purplegoolabs.com>
2012-08-26 14:18:04 +0200

    updated the version of Postal dependency

--- project.clj
+++ project.clj
@@ -38,7 +38,7 @@
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.2"]
                            [congomongo "0.1.11-SNAPSHOT"]
-                           [com.draines/postal "1.7.1"]
+                           [com.draines/postal "1.8.0"]
                            [clj-time "0.4.2"]
                            [jayq "0.1.0-alpha4"]
                            [fetch "0.1.0-alpha2"]]
